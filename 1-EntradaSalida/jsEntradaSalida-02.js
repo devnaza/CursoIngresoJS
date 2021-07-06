eac4dea9c743ae73b2b3e0acdd544192ab307b84
Naza <devnaza@gmail.com>
2021-07-05 22:19:39 -0300

    Ejercicios del 1 al 5.

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,15 @@
 /*
+Autor: Nazareno Santa Cruz
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
-}
+	var nombre;
+	var mensaje;
 
+	nombre = prompt("Ingrese su nombre", "Escribí sobre esta línea");
+
+	mensaje = "Su nombre es: " + nombre;
+
+	alert(mensaje);
+}
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,18 @@
 /* 
 Debemos lograr tomar un dato por 'PROMPT' 
-y lo muestro por 'ID' al presionar el botón 'mostrar'
+y lo muestro por 'ID' al presionar el botón 'mostrar' /// había un com 
 */
 function mostrar()
 {
-	alert("ok");
-}
+	var datoRequerido;
 
+	var mensaje;
+
+	datoRequerido = prompt("Mencioná un dato aleatorio", "Escribilo acá");
+
+	mensaje = "Su nombre es: " + datoRequerido;
+
+	document.getElementById('txtIdNombre').value = mensaje;
+
+	// document.getElementById('txtIdNombre').value = "Hola";
+}
